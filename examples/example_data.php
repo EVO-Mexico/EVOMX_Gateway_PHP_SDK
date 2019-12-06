bf80ffff12141124d8ce4b51d8df9a0771b2585d
kieranmurray <kieran.murray@boipa.com>
2019-12-06 16:36:32 +0100

    remove sample merchant data

--- examples/example_data.php
+++ examples/example_data.php
@@ -11,16 +11,6 @@ $currency = "";
 $credit_card_token = "8399296648645454";
 $customer_it_token = "ETsAsTWs69NbNovf0JOw";
 
-/*
-$merchantId = "5000";
-$password = "5678";
-$brandId = "";
-$country = "GB";
-$currency = "EUR";
-$credit_card_token = "3849345322385454";
-$customer_it_token = "8Gii57iYNVSd27xnFZzR";
-*/
-
 $credit_card_number = "5454545454545454";
 $credit_card_expiry_month = "12";
 $credit_card_expiry_year = "2021";
